60c2dd4de8981c738c8555ab4a05917c1f11c80f
Administrator <Administrator@172.31.14.155>
2016-04-20 06:06:54 +0000

    wedvr

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -15,7 +15,7 @@
 
 # ELB_LIST defines which Elastic Load Balancers this instance should be part of.
 # The elements in ELB_LIST should be seperated by space.
-ELB_LIST="http://VehicleRe-PublicEl-WNCUGUY63RAQ-1962691640.ap-southeast-2.elb.amazonaws.com"
+ELB_LIST="http://VehicleRe-PublicEl-15KA2YDV75EPP-281953503.ap-southeast-2.elb.amazonaws.com"
 
 # Under normal circumstances, you shouldn't need to change anything below this line.
 # -----------------------------------------------------------------------------
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@
 # Start web server
 #service httpd start
 docker pull nijisha/mt-mvn-vr-presentation:latest
-docker run --name web -d -p 8080:8080 -e BUSINESS_IP=http://VehicleRe-Business-OAC5PR6YYOGP-708637383.ap-southeast-2.elb.amazonaws.com/VehicleBusiness/vehiclerental nijisha/mt-mvn-vr-presentation:latest
+docker run --name web -d -p 8080:8080 -e BUSINESS_IP=http://VehicleRe-Business-IMPZBT8JUZSN-535377960.ap-southeast-2.elb.amazonaws.com/VehicleBusiness/vehiclerental nijisha/mt-mvn-vr-presentation:latest
