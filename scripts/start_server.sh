940589d495a2346fd5d855b691d57ae0b21f90b3
Administrator <Administrator@172.31.14.155>
2016-04-29 09:52:35 +0000

    we

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@
 # Start web server
 #service httpd start
 docker pull nijisha/mt-mvn-vr-presentation:latest
-docker run --name web -d -p 8080:8080 -e BUSINESS_IP=http://VehicleRe-Business-IMPZBT8JUZSN-535377960.ap-southeast-2.elb.amazonaws.com/VehicleBusiness/vehiclerental nijisha/mt-mvn-vr-presentation:latest
+docker run --name web -d -p 8080:8080 -e BUSINESS_IP=http://VehicleRe-Business-16KNJM8QMJ2N0-610794444.ap-southeast-2.elb.amazonaws.com/VehicleBusiness/vehiclerental nijisha/mt-mvn-vr-presentation:latest
